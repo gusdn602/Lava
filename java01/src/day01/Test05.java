9417405af19a87e305c9aec71057a8fd2928da51
Class02 <Class02@DESKTOP-OVM0OQG>
2020-03-09 18:06:58 +0900

    2020.03.09

---
+++ java01/src/day01/Test05.java
@@ -0,0 +1,18 @@
+package day01;
+
+/**
+ * 이 클래스는 연산자를 연습하기 위한 클래스
+ * @author 우현우
+ *@since 2020.03.09
+ *@version v.1.0
+ *@see
+ *		변경이력
+ *			2020.03.09	-	클래스제작 - 담당자 : 우현우
+ */
+public class Test05 {
+	public static void main (String[] args) {
+		System.out.println("10 << 2 :" + (10 <<2));
+		System.out.println("-10 >> 2 :" + (-10 >> 2));
+		System.out.println("-10 >>> 2 :" + (-10 >>> 2));
+	}
+}
